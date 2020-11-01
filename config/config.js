15aa025c9734ddc6dd01cb36b7e1a4b371f47bac
Kerem <kerematam@gmail.com>
2020-11-02 00:29:31 +0300

    fix: integration to atlas db & upgrade mongoose version

--- config/config.js
+++ config/config.js
@@ -1,28 +1,27 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 // require and configure dotenv, will load vars in .env in PROCESS.ENV
-require('dotenv').config();
+require("dotenv").config();
 
 // define validation for all the env vars
 const envVarsSchema = Joi.object({
   NODE_ENV: Joi.string()
-    .allow('development', 'production', 'test', 'provision')
-    .default('development'),
-  PORT: Joi.number()
-    .default(4040),
-  MONGOOSE_DEBUG: Joi.boolean()
-    .when('NODE_ENV', {
-      is: Joi.string().equal('development'),
-      then: Joi.boolean().default(true),
-      otherwise: Joi.boolean().default(false)
-    }),
-  JWT_SECRET: Joi.string().required()
-    .description('JWT Secret required to sign'),
-  MONGO_HOST: Joi.string().required()
-    .description('Mongo DB host url'),
-  MONGO_PORT: Joi.number()
-    .default(27017)
-}).unknown()
+    .allow("development", "production", "test", "provision")
+    .default("development"),
+  PORT: Joi.number().default(4040),
+  MONGOOSE_DEBUG: Joi.boolean().when("NODE_ENV", {
+    is: Joi.string().equal("development"),
+    then: Joi.boolean().default(true),
+    otherwise: Joi.boolean().default(false),
+  }),
+  JWT_SECRET: Joi.string()
+    .required()
+    .description("JWT Secret required to sign"),
+  MONGO_HOST: Joi.string().required().description("Mongo DB host url"),
+  MONGO_PORT: Joi.number().default(27017),
+  MONGODB_URI: Joi.string().required().description("Mongo DB full url"),
+})
+  .unknown()
   .required();
 
 const { error, value: envVars } = envVarsSchema.validate(process.env);
@@ -36,9 +35,10 @@ const config = {
   mongooseDebug: envVars.MONGOOSE_DEBUG,
   jwtSecret: envVars.JWT_SECRET,
   mongo: {
+    uri: envVars.MONGODB_URI,
     host: envVars.MONGO_HOST,
-    port: envVars.MONGO_PORT
-  }
+    port: envVars.MONGO_PORT,
+  },
 };
 
 module.exports = config;
