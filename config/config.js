e2c7c50798a319c1fcad88bf6d3f67576cd4f503
Kerem <kerematam@gmail.com>
2020-11-02 01:13:05 +0300

    chore: add default port for heroku

--- config/config.js
+++ config/config.js
@@ -3,12 +3,14 @@ const Joi = require("joi");
 // require and configure dotenv, will load vars in .env in PROCESS.ENV
 require("dotenv").config();
 
+const defaultPort = process.env.PORT || 4040
+
 // define validation for all the env vars
 const envVarsSchema = Joi.object({
   NODE_ENV: Joi.string()
     .allow("development", "production", "test", "provision")
     .default("development"),
-  PORT: Joi.number().default(4040),
+  PORT: Joi.number().default(defaultPort),
   MONGOOSE_DEBUG: Joi.boolean().when("NODE_ENV", {
     is: Joi.string().equal("development"),
     then: Joi.boolean().default(true),
